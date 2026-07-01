01344cf67498fcba3a8d13abafd7d87eda5b8258
Quentin Hacourt <quentin.hacourt@gmail.com>
2026-07-01 11:31:02 +0200

    test subject done

---
+++ entropy.lisp
@@ -0,0 +1,42 @@
+(defpackage :entropy
+  (:use :cl))
+
+(in-package :entropy)
+
+(defun unique-symbols (str)
+  "Finds all the different symbols in a list and returns them as a list."
+  (let ((symbols '()))
+    (loop for char across str
+          unless (member char symbols)
+                 (push char symbols)))
+  symbols))
+
+(defun count-appearances (symbols str)
+  "Counts how often each symbol in the list appears in the original string."
+  (loop for symbol in symbols
+        collect (list symbol
+                      (count symbol str :test #'eql))))
+
+(defun str-len (str)
+  "Counts the length of a string."
+  (let ((ctr 0))
+    (loop for char across str
+          do (incf ctr))
+    ctr))
+
+(defun compute-probabilities (frequencies len)
+  "given the frequencies and the total length, computes the probability of a symbol appearing."
+  (loop for pair in frequencies
+        for symbol = (first pair)
+        for count = (second pair)
+        collect (list symbol (/ (float count) len))))
+
+(defun compute-entropy (str)
+  "Computes the entropy of any given string."
+  (let* ((symbols (unique-symbols str))
+         (frequencies (count-appearances symbols str))
+         (len (str-len str))
+         (probabilities (compute-probabilities frequencies len))
+         (sum 0))
+    (- (loop for (symbol p) in probabilities
+             sum (* p (log p 2))))))
